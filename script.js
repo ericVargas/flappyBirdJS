325dc239368f44aa7f340c4901d5af5f2bfc85d9
ericVargas <batman@Erics-MacBook-Pro-2.local>
2018-11-10 17:21:02 -0500

    created canvas, styled font and loaded images

---
+++ script.js
@@ -0,0 +1,15 @@
+let cvs = document.getElementById("canvas");
+let ctx = cvs.getContext("2d");
+
+// Loading images
+let bird = new Image();
+let bg = new Image();
+let fg = new Image();
+let pipeT = new Image();
+let pipeB = new Image();
+
+bird.src = "img/bird.png";
+bg.src = "img/bg.png";
+fg.src = "img/fg.png";
+pipeT.src = "img/pipeTop.png";
+pipeB.src = "img/pipeBottom.png";
