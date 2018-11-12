fafdbf4fe5250e47446834a977b4466e7efc4c92
ericVargas <batman@Erics-MacBook-Pro-2.local>
2018-11-12 01:22:17 -0500

    adding score display and sound

--- script.js
+++ script.js
@@ -23,11 +23,21 @@ let bY = 150;
 
 let gravity = 1.5;
 
+let score = 0;
+
+// Sounds
+let fly = new Audio();
+let oneUp = new Audio();
+
+fly.src = "sound/fly.mp3";
+oneUp.src = "sound/score.mp3";
+
 // When key down
 document.addEventListener("keydown", moveUp);
 
 function moveUp(){
     bY -= 25;
+    fly.play();
 }
 
 // Pipe coordinates
@@ -59,8 +69,13 @@ function draw(){
         }
         
         // Detecting collision
-        if(bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeT.width && (bY <= pipe[i].y + pipeT.height || bY+bird.height >= pipe[i].y+constant)){
+        if(bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeT.width && (bY <= pipe[i].y + pipeT.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >= cvs.height - fg.height){
             location.reload(); // reload page
+        }
+        
+        if(pipe[i].x == 5){
+            score++;
+            oneUp.play();
         }
     }
     
@@ -69,6 +84,11 @@ function draw(){
     ctx.drawImage(bird,bX,bY);
     
     bY += gravity;
+    
+    ctx.fillStyle = "#000";
+    ctx.font = "20px Verdana";
+    ctx.fillText("Score : "+score,10,cvs.height-20);
+    
     requestAnimationFrame(draw);
 }
 
